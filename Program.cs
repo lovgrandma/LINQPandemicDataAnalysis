304627a2e16156750f9b3ff614e8b3d5955e2d9f
Jesse <jessealexanderthompson@gmail.com>
2021-05-19 10:22:41 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_W2021_991577657
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App app = new App();
+            app.runFinal();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# F_W2021_991577657
+ My C# class final using CRUD LINQ operations to update Pandemic Data
